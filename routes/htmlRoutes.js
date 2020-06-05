f2f964fcfef007f2db77b5f5ea826311b7ec3e33
Vanessa Pham <phamxvanessa@gmail.com>
2020-06-05 01:37:58 -0600

    push to heroku again

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const router = require('express').Router();
 
-router.get('/notes', function(req, res) {
+router.get('/notes', function (req, res) {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
 })
 
-router.get('*', function(req, res) {
+router.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
