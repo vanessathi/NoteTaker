2117d42a46162974a6ce5e723a7bbe36da1d299a
Vanessa Pham <phamxvanessa@gmail.com>
2020-06-05 01:52:24 -0600

    typo in store.js

--- db/store.js
+++ db/store.js
@@ -8,10 +8,11 @@ const writeFileAsync = util.promisify(fs.writeFile);
 class Store {
     read(){
         return readFileAsync('db/db.json', 'utf8');
-    }
+    };
+
     write(note){
         return writeFileAsync('db/db.json', JSON.stringify(note));
-    }
+    };
 
     getNotes(){
         return this.read().then(notes => {
@@ -33,15 +34,15 @@ class Store {
         }
     const newNote = { title, text, id: uuidv1() }
 
-    return this.getNotes()
+    return this.getNotes().then(notes => [...notes, newNote])
     .then(updatedNotes => this.write(updatedNotes))
-    .then(() => newNotes)
+    .then(() => newNote)
     }
 
     removeNote(id) {
         return this.getNotes()
-            .then(notes => notes.filter(not=> note.id !== id))
-            .then(filteredNotes => this.write(filteredNotes))
+            .then(notes => notes.filter(note => note.id !== id))
+            .then(filteredNotes => this.write(filteredNotes));
     }
 }
 
