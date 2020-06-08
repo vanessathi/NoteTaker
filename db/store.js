6f8fac02bcbc181820a715239ab1436144baed3c
Vanessa Pham <phamxvanessa@gmail.com>
2020-06-08 08:57:20 -0600

    set up deploy

--- db/store.js
+++ db/store.js
@@ -1,6 +1,5 @@
 const util = require("util");
 const fs = require("fs");
-const uuidv1 = require("uuid/v1");
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const path = require("path")
-const fs = require("fs")
-const apiRoutes = require("./routes/apiRoutes")
-const htmlRoutes = require("./routes/htmlRoutes")
+const path = require("path");
+const fs = require("fs");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
